9f1f2ba524f5bf194e28c511b9994fb98c81b2af
fatehah <suziazlin120194@gmail.com>
2015-10-11 13:35:33 -0400

    setsockopt&getsockopt

--- server2.py
+++ server2.py
@@ -1,16 +1,24 @@
 import socket
 import time
 
-serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+from socket import *
+from time import *
+serversocket = socket(AF_INET,SOCK_STREAM)
 
-host = socket.gethostname()
+host = gethostname()
 port = 5000
 serversocket.bind((host,port))
 serversocket.listen(5)
-
+get = ''
 while True:
 	clientsocket,addr = serversocket.accept()
 	print("GOT THE CONNECTION FROM	%s" % str(addr))
-	currentTime = time.ctime(time.time()) + "\r\n"
+
+	get = serversocket.getsockopt(SOL_SOCKET,SO_KEEPALIVE)
+	print "GET  SOCKET KEEPALIVE : ",get;
+	set = serversocket.setsockopt(SOL_SOCKET,SO_KEEPALIVE,1)  
+	print "SET SOCKET KEEPALIVE  : ",serversocket.getsockopt(SOL_SOCKET,SO_KEEPALIVE)
+
+	currentTime = ctime(time()) + "\r\n"
 	clientsocket.send(currentTime.encode('ascii'))
 	clientsocket.close()
